002b4118ccb1d0a21f80428caacde92300022291
Dylan <kite.d92@gmail.com>
2018-12-04 15:51:36 -0800

    added signout

--- scripts/nav.js
+++ scripts/nav.js
@@ -8,7 +8,17 @@ function init(){
         })
         .then((result) => {
             document.querySelector('.welcome').textContent += ` ${result.data[0].first_name}!`
+            document.querySelector('.signoutDiv p').addEventListener('click', signout)
         })
+        
 }
+
+function signout(){
+    localStorage.removeItem('token')
+    localStorage.removeItem('uId')
+    window.location.pathname = '/'
+}
+
+
 
 module.exports = {init}
