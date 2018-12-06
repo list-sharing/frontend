aaf702bd8911ef8130099a0971d8f7edc8c5745d
Dylan <kite.d92@gmail.com>
2018-12-05 18:32:50 -0800

    populated edit form with list details

--- scripts/edit.js
+++ scripts/edit.js
@@ -1,8 +1,32 @@
+const listOperations = require('./listOperations')
+const {axios} = require('./utils')
+
 function init(){
     const listId = localStorage.getItem('edit')
-    
-    console.log(listId)
-    // return axios(`/users/${userId}`)
+    if(!listId) return listOperations.init()
+    const userId = localStorage.getItem('uId')
+    console.log(listId, userId)
+    return fillForm(userId, listId)
 }
+
+function fillForm(userId, listId){
+    return axios(`/users/${userId}/lists/${listId}`)
+    .then(result => {
+        console.log(result)
+        addListInfo(result.data[0])
+        return axios(`/users/${userId}/lists/${listId}/items`)
+    })
+    .then(results => console.log(results))
+}
+
+function addListInfo(list){
+    document.querySelector('#list_name').value = list.list_name
+    document.querySelector('#img').value = list.img
+    document.querySelector('header').style.backgroundImage = `url('${list.img}')`
+    document.querySelector('#listContainer').setAttribute('data-id', list.id)
+    document.querySelector('textarea').value = list.desc
+}
+
+function addItemInfo(itemArr){}
 
 module.exports = {init}
