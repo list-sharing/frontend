7ba57cfd96f0968a285629cc6b141e239798e584
Dylan <kite.d92@gmail.com>
2018-12-05 19:05:11 -0800

    made cancel button

--- scripts/edit.js
+++ scripts/edit.js
@@ -1,22 +1,26 @@
 const listOperations = require('./listOperations')
 const {axios} = require('./utils')
+const {editableItemTemplate} = require('./templates')
 
 function init(){
     const listId = localStorage.getItem('edit')
     if(!listId) return listOperations.init()
     const userId = localStorage.getItem('uId')
-    console.log(listId, userId)
     return fillForm(userId, listId)
 }
 
 function fillForm(userId, listId){
-    return axios(`/users/${userId}/lists/${listId}`)
-    .then(result => {
-        console.log(result)
-        addListInfo(result.data[0])
-        return axios(`/users/${userId}/lists/${listId}/items`)
+    
+    return axios(`/users/${userId}/lists/${listId}/items`)
+    .then(results => {
+        addItemInfo(results.data)
+        prepButtons()
+        return axios(`/users/${userId}/lists/${listId}`)
     })
-    .then(results => console.log(results))
+    .then(result => {
+        addListInfo(result.data[0])    
+    })
+    
 }
 
 function addListInfo(list){
@@ -27,6 +31,23 @@ function addListInfo(list){
     document.querySelector('textarea').value = list.desc
 }
 
-function addItemInfo(itemArr){}
+function addItemInfo(itemArr){
+    console.log(itemArr)
+    document.querySelector('#new').remove()
+    document.querySelector('.field:last-of-type').remove()
+    const inputArr = []
+    itemArr.forEach(item => inputArr.push(editableItemTemplate(item)))
+    document.querySelector('.stacked.segment').innerHTML += inputArr.join('')
+}
 
+function prepButtons(){
+    document.querySelector('#submit').textContent = "change"
+    document.querySelector('#listForm').innerHTML += '<button id="cancel" type="button" class="ui fluid large grey submit button">cancel</button>'
+    document.querySelector('#cancel').addEventListener('click', cancel)
+}
+
+function cancel(){
+    localStorage.removeItem('edit')
+    window.location.pathname = '/listPage/listPage.html'
+}
 module.exports = {init}
